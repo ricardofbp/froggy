d944901c5e8df8003b92fa2a4630df585894ed41
ricardofbp <ricardobaiapereira@gmail.com>
2018-07-13 16:34:48 +0100

    updated frogger.js

--- frogger/frogger.js
+++ frogger/frogger.js
@@ -1,7 +1,17 @@
-function setup() {
-  var cnv = createCanvas(100, 100);
+var cnv;
+
+function centerCanvas() {
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
+}
+
+function setup() {
+  cnv = createCanvas(100, 100);
+  centerCanvas();
   background(255, 0, 200);
+}
+
+function windowResized() {
+  centerCanvas();
 }
