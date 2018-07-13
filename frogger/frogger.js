857b4c00ad540172b5b66797625f7b240841efd4
AndreOA <a.atalaia@campus.fct.unl.pt>
2018-07-13 18:09:57 +0100

    updated frogger.js

--- frogger/frogger.js
+++ frogger/frogger.js
@@ -1,4 +1,5 @@
 var cnv;
+var frog;
 const WIDTH = 400;
 const HEIGHT = 400;
 
@@ -10,20 +11,43 @@ function centerCanvas() {
 }
 
 function play() {
-  var s = 'The quick brown fox jumped over the lazy dog.';
+  var s = 'PRESS F to PLAY';
   fill(255);
   translate(WIDTH/2 - 45, HEIGHT/2 - 45);
   text(s, 10, 10, 80, 80); // Text wraps within text box
+  
 }
 
 function setup() {
   cnv = createCanvas(WIDTH, HEIGHT);
   centerCanvas();
 	background("#222222");
-  play();
+  playScreen();
 }
 
 function windowResized() {
   centerCanvas();
 }
 
+
+function keyPressed(){
+  if (keyCode  === 70){
+    clear();
+}
+}
+
+function playScreen() {
+  var begin = 'PRESS SPACEBAR TO PLAY';
+  var title = "FROGGER"
+  fill(255);
+  push();
+  translate(WIDTH/2 - 100, HEIGHT/2 - 100);
+  textSize(40);
+  text(title, 0, 0, 50, 100);
+  pop();
+  push();
+  translate(WIDTH/2 - 135, HEIGHT/2 + 30);
+  textSize(20);
+  text(begin, 0, 0, 280, 40); // Text wraps within text box
+  pop();
+}
