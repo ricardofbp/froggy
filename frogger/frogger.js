4cd973f096184be4bcc25c981dd5f930ae91c728
ricardofbp <ricardobaiapereira@gmail.com>
2018-07-14 19:12:49 +0100

    reverted graphics

--- frogger/frogger.js
+++ frogger/frogger.js
@@ -15,15 +15,10 @@ const SCL = 30; /*tem que ser multipla de WIDTH, HEIGHT*/
 var cnv;
 var player;
 var inMenu = true;
-var img;
 
 /***********************************************/
 /*                FUNCOES                      */
 /***********************************************/
-
-function preload() {
-  img = loadImage('../assets/crazy_frog.png');
-}
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -112,7 +107,6 @@ function keyPressed() {
     if (keyCode === LEFT_ARROW)  player.move(-1,0);
     if (keyCode === RIGHT_ARROW) player.move(1,0);
   }
-  console.log("rotation: " + player.rotation);
   
 }
 
@@ -142,21 +136,17 @@ function Frog() {
   this.x = WIDTH/2 - SCL/2;
   this.y = HEIGHT - SCL;
   this.score = 0;
-  this.rotation = 0;
   
   /*receives 1 or 0 (can be negative) and summs to the current position*/
   /*constrain constrains the min and max value the player's position can have*/
   this.move = function(x, y) {
-    this.rotation = x*PI;
     this.x = constrain(this.x + (x * SCL), 0, WIDTH - SCL);
     this.y = constrain(this.y + (y * SCL), 0, HEIGHT - SCL);
   }
   
   this.show = function() {
-    //fill(0, 240, 0);
-    image(img, this.x, this.y, SCL, SCL);
-    pop();
-
+    fill(0, 240, 0);
+    rect(this.x, this.y, SCL, SCL);
   }
   
   this.reset = function() {
