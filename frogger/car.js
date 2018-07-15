f1ad737bba8f6fefd7fc130d7693e70bfb93a4df
ricardofbp <ricardobaiapereira@gmail.com>
2018-07-15 19:30:27 +0100

    fixed car movement

--- frogger/car.js
+++ frogger/car.js
@@ -1,28 +1,29 @@
 class Car {
   constructor(y) {
-    this.size = SCL;
-    this.x1 = 0;
+    this.width = SCL*2;
+    this.height = SCL;
+    this.x1 = 0 - this.width;
     this.y1 = y;
-    this.x2 = this.x1 + this.size*2;
-    this.y2 = this.y1 + this.size;
+    this.x2 = this.x1 + this.width;
+    this.y2 = this.y1 + this.height;
     this.speed = 1;
   }
   
   update() {
-    if(this.x1 == WIDTH + this.size) this.reset();
+    if(this.x1 == WIDTH + this.width) this.reset();
     this.x1 += this.speed;
     this.x2 += this.speed;
   }
   
   /*no need to reset y 'cus its always the same*/
   reset() {
-    this.x1 = 0 - this.size;
-    this.x2 = this.x1 + this.size;
+    this.x1 = 0 - this.width;
+    this.x2 = this.x1 + this.width;
   }
   
   show() {
     fill(100, 0, 0);
-    rect(this.x1, this.y1, this.size*2, this.size);
+    rect(this.x1, this.y1, this.width, this.height);
   }
   
 }
